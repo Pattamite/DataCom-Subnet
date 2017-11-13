d791a3482184b77bfff12a647083a7cb49ccfbaa
Pattnatt <jittapat007@gmail.com>
2017-11-13 21:30:04 +0700

    Add data to info_table

--- SubnetJTP/SubnetWeb/views.py
+++ SubnetJTP/SubnetWeb/views.py
@@ -9,10 +9,15 @@ def index(request):
 
     if form.is_valid():
         network_class = form.cleaned_data['network_class']
-        subnet_mask = form.cleaned_data['subnet_mask']
+        subnet_num = int(form.cleaned_data['subnet_mask'])
         ip4_input = form.cleaned_data['ip4_input']
+        subnet = form.SUBNET_MASK[str(subnet_num)]
+        wildcard_num = 32 - subnet_num
 
-        subnet = form.SUBNET_MASK[subnet_mask]
+        wildcard = []
+        for i in range(0, 4):
+            wildcard.append(255 - subnet[i])
+
         ip4 = []
         for string in ip4_input.split('.') :
             ip4.append(int(string))
@@ -21,9 +26,22 @@ def index(request):
         for i in range(0, 4):
             net_addr.append(subnet[i] & ip4[i])
 
+        broadcast_addr = net_addr[:]
+        for i in range(0, 4):
+            broadcast_addr[i] |=  wildcard[i]
+
+
         info_table = []
-        info_table.append(['IP Address', ip4_input])
-        info_table.append(['Network Address', str(net_addr[0]) + '.' + str(net_addr[1]) + '.' + str(net_addr[2]) + '.' + str(net_addr[3])])
+        info_table.append(['IP Address', makeIpString(ip4)])
+        info_table.append(['Network Address', makeIpString(net_addr)])
+        info_table.append(['Usable Host IP Range', usableHostRange(net_addr, broadcast_addr, subnet_num)])
+        info_table.append(['Broadcast Address', makeIpString(broadcast_addr)])
+        info_table.append(['Total Number of Hosts', str(2 ** wildcard_num)])
+        info_table.append(['Number of Usable Hosts', str(max([(2 ** wildcard_num) - 2, 0]))])
+        info_table.append(['Subnet Mask', makeIpString(subnet)])
+        info_table.append(['Wildcard Mask', makeIpString(wildcard)])
+        info_table.append(['Binary Subnet Mask', makeBinaryIpString(subnet)])
+        info_table.append(['Binary Wildcard Mask', makeBinaryIpString(wildcard)])
 
     context = {
     'form' : form,
@@ -32,3 +50,33 @@ def index(request):
     }
     template = 'index.html'
     return render(request, template, context)
+
+def makeIpString(value):
+    ans = ''
+    if(len(value) > 0):
+        ans = str(value[0])
+        if(len(value) > 1):
+            for i in range(1, len(value)):
+                ans += ('.' + str(value[i]))
+    return ans
+
+def usableHostRange(net_addr, broadcast_addr, subnet_num):
+    if subnet_num >= 31 :
+        return 'None'
+    else :
+        min_addr = net_addr[:]
+        min_addr[3] += 1
+
+        max_addr = broadcast_addr[:]
+        max_addr[3] -= 1
+
+        return makeIpString(min_addr) + " - " + makeIpString(max_addr)
+
+def makeBinaryIpString(value):
+    ans = ''
+    if(len(value) > 0):
+        ans = '{0:08b}'.format(value[0])
+        if(len(value) > 1):
+            for i in range(1, len(value)):
+                ans += ('.' + '{0:08b}'.format(value[i]))
+    return ans
